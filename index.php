b2cdd3eb58cd73b68eac0343dcd948a86aa02c87
nstolk <nick_stolk9@hotmail.com>
2019-09-30 16:04:15 +0200

    Add gitignore

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require 'vendor/autoload.php';
 require 'core/bootstrap.php';
 
-// Can either be seperated or combined with curly braces
+// Can either be separated or combined with curly braces
 use App\Core\{Router, Request};
 
 Router::load('app/routes.php')
